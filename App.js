6844a481c98058a6af0d7edf7d9c10cfb2bdbb33
Audrey Bediako <fdu5py@virginia.edu>
2025-02-22 21:06:33 -0500

    installed backend of firebase on inside jsons

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hey there!</Text>
+      <Text>Hidden Guide</Text>
       <StatusBar style="auto" />
     </View>
   );
