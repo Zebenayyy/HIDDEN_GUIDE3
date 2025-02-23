ed26887013ad93fab9db2cefb07c194793bcfce6
Audrey Bediako <fdu5py@virginia.edu>
2025-02-23 02:59:22 -0500

    didn't do anything the expo is just broken whatever

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import HomeScreen from './screens/HomeScreen'; // Import the Homepage
-import LogIn from './screens/LogIn'; // Login page
-import Registration from './screens/Registration'; // Registration page
-import ForgotPasswordScreen from './screens/ForgotPasswordScreen'; // Forgot Password page
-import ExploreScreen from './screens/ExploreScreen'; // Explore page
+import HomeScreen from './screens/HomeScreen.js'; // Import the Homepage
+import LogIn from './screens/LogIn.js'; // Login page
+import Registration from './screens/Registration.js'; // Registration page
+import ForgotPasswordScreen from './screens/ForgotPasswordScreen.js'; // Forgot Password page
+import ExploreScreen from './screens/ExploreScreen.js'; // Explore page
 
 const Stack = createStackNavigator();
 
